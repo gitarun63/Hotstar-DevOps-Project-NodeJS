abebb36177f1566de205c6fb7664fb266df7f1bd
gitarun63 <169031084+gitarun63@users.noreply.github.com>
2024-08-30 17:09:40 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mynewhotstarcicdproject" # Replace with your actual S3 bucket name
+    bucket = "mynewhotstarcicdproject1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
